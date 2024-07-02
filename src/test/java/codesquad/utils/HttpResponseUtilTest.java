9dd265cdb1f23055ee35792b3c83c150293866d3
kariskan <zkfzpf56@naver.com>
2024-07-03 02:50:19 +0900

    test: test 추가

---
+++ src/test/java/codesquad/utils/HttpResponseUtilTest.java
@@ -0,0 +1,33 @@
+package codesquad.utils;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Map;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import codesquad.domain.HttpHeader;
+import codesquad.domain.HttpProtocol;
+import codesquad.domain.HttpResponse;
+import codesquad.domain.HttpStatus;
+import codesquad.domain.StatusLine;
+
+public class HttpResponseUtilTest {
+
+	@Test
+	@DisplayName("HttpResponse 쓰기 테스트")
+	void testWriteResponse() throws Exception {
+		StatusLine statusLine = new StatusLine(HttpProtocol.HTTP11, HttpStatus.OK);
+		HttpHeader header = new HttpHeader(Map.of("Content-Type", "text/html"));
+		String body = "<html><body>Hello World</body></html>";
+		HttpResponse httpResponse = new HttpResponse(statusLine, header, body);
+
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		HttpResponseUtil.writeResponse(outputStream, httpResponse);
+
+		String expectedResponse = statusLine.toString() + header.toString() + body;
+		assertThat(outputStream.toString()).isEqualTo(expectedResponse);
+	}
+}
