d943e48af96644af0867b4e57b72253cfcde641c
kariskan <zkfzpf56@naver.com>
2024-07-07 21:26:05 +0900

    fix: 정적 자원 먼저 조회 후 동적 핸들러 호출

--- src/main/java/codesquad/handler/HandlerMapping.java
+++ src/main/java/codesquad/handler/HandlerMapping.java
@@ -1,8 +1,12 @@
 package codesquad.handler;
 
+import java.net.URL;
+
 import codesquad.domain.HttpRequest;
 
 public class HandlerMapping {
+
+	public static final String STATIC_PATH = "static";
 
 //	private static final Map<String, Handler> DYNAMIC_HANDLERS = Map.of(
 //		new RequestLine(HttpMethod.GET, new Path("/create"), HttpProtocol.HTTP11), SignUpHandler.getInstance()
@@ -13,9 +17,17 @@ public class HandlerMapping {
 	}
 
 	public static Handler getHandler(HttpRequest request) {
+		URL resource = HandlerMapping.class.getClassLoader().getResource(STATIC_PATH + request.requestLine().getUrl());
+		if (resource == null) {
+			return getDynamicHandler(request);
+		}
+		return StaticRequestHandler.getInstance();
+	}
+
+	public static Handler getDynamicHandler(HttpRequest request) {
 		if (request.isGet() && "/create".equals(request.requestLine().getUrl())) {
 			return SignUpHandler.getInstance();
 		}
-		return StaticRequestHandler.getInstance();
+		throw new IllegalArgumentException("Invalid request");
 	}
 }
