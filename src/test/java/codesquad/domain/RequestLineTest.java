02b236464487a121d7b1f26e043a62a0c75fc6b1
kariskan <zkfzpf56@naver.com>
2024-07-07 21:49:48 +0900

    test: 테스트 수정

--- src/test/java/codesquad/domain/RequestLineTest.java
+++ src/test/java/codesquad/domain/RequestLineTest.java
@@ -10,8 +10,8 @@ public class RequestLineTest {
 	@Test
 	@DisplayName("RequestLine을 생성하고 URL을 반환한다")
 	void createRequestLine() {
-		Path path = new Path("/index.html");
+		Path path = new Path("/register.html");
 		RequestLine requestLine = new RequestLine(HttpMethod.GET, path, HttpProtocol.HTTP11);
-		assertThat(requestLine.getUrl()).isEqualTo("/index.html");
+		assertThat(requestLine.getUrl()).isEqualTo("/register.html");
 	}
 }
