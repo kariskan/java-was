8101646899149569d7e73bf0fec66a7eb1f55a5d
kariskan <zkfzpf56@naver.com>
2024-07-16 19:33:13 +0900

    feat: response null check

--- src/main/java/codesquad/utils/HttpResponseUtil.java
+++ src/main/java/codesquad/utils/HttpResponseUtil.java
@@ -1,9 +1,8 @@
 package codesquad.utils;
 
+import codesquad.domain.HttpResponse;
 import java.io.IOException;
 import java.io.OutputStream;
-
-import codesquad.domain.HttpResponse;
 
 public class HttpResponseUtil {
 
@@ -11,7 +10,13 @@ public class HttpResponseUtil {
 	}
 
 	public static void writeResponse(OutputStream bo, HttpResponse response) throws IOException {
+		if (response.getStatusLine() == null) {
+			response.setStatusLine();
+		}
 		bo.write(response.getStatusLine().toString().getBytes());
+		if (response.getBody() != null) {
+			response.addHeader("Content-Length", String.valueOf(response.getBody().length));
+		}
 		bo.write(response.getHeader().toString().getBytes());
 		if (response.getBody() != null) {
 			bo.write(response.getBody());
